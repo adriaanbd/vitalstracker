eb5af2f204d2fa5b0017f5e6021ad0b2cf081e01
adriaanbd <adriaanbd@gmail.com>
2020-03-12 18:44:53 -0500

    linter updates

--- src/components/FooterItem.js
+++ src/components/FooterItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function FooterItem(props) {
-	return (
-		<div className="footer-item">
-			<div>{props.icon}</div>
-			<div>{props.title}</div>
-		</div>
-	)
+  return (
+    <div className="footer-item">
+      <div>{props.icon}</div>
+      <div>{props.title}</div>
+    </div>
+  );
 }
 
 export default FooterItem;
