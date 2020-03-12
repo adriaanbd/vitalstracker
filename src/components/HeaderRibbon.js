6c86c2a6e79b4e0a716e40634684d7e82acc9d68
adriaanbd <adriaanbd@gmail.com>
2020-03-11 22:32:32 -0500

    Added header nav comp

---
+++ src/components/HeaderRibbon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/HeaderRibbon.css';
+
+function HeaderRibbon(props) {
+	return (
+    <div className="ribbon">
+      <h1>{props.title ? props.title : "Track.It"}</h1>
+    </div>
+  )
+};
+
+export default HeaderRibbon;
