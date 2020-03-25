69770d59960a6166d978beff8a017b4b18615197
adriaanbd <adriaanbd@gmail.com>
2020-03-25 16:55:04 -0500

    header ribbon needs to be dynamic per page

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,35 @@ import TrackitCards from './TrackitCards';
 import ProgressPage from './ProgressPage';
 import './styles/App.css';
 import ProfilePage from './ProfilePage';
+import HeaderRibbon from './HeaderRibbon';
 
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/"><Menu /></Route>
-        <Route path="/measurement"><VitalCards /></Route>
-        <Route path="/tracker"><TrackitCards /></Route>
-        <Route path="/progress"><ProgressPage /></Route>
-        <Route path="/menu"><Menu /></Route>
-        <Route path="/profile"><ProfilePage /></Route>
+        <Route exact path="/">
+          <HeaderRibbon />
+          <VitalCards />
+        </Route>
+        <Route path="/measurement">
+          <HeaderRibbon />
+          <VitalCards />
+        </Route>
+        <Route path="/tracker">
+          <HeaderRibbon />
+          <TrackitCards />
+        </Route>
+        <Route path="/progress">
+          <HeaderRibbon />
+          <ProgressPage />
+        </Route>
+        <Route path="/menu">
+          <HeaderRibbon title='More'/>
+          <Menu />
+        </Route>
+        <Route path="/profile">
+          <ProfilePage />
+        </Route>
       </Switch>
       <NavFooter />
     </>
