aaa749bdb074db6e524dd5a7e21875cd59698bf5
adriaanbd <adriaanbd@gmail.com>
2020-03-24 23:25:24 -0500

    added router, switch and route statements

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import VitalCards from './VitalCards';
 import NavFooter from './NavFooter';
+import Menu from './Menu';
+import TrackitCards from './TrackitCards';
+import ProgressPage from './ProgressPage';
 import './styles/App.css';
 import ProfilePage from './ProfilePage';
 
 function App() {
   return (
     <>
-      <ProfilePage />
+      <Switch>
+        <Route exact path="/"><Menu /></Route>
+        <Route path="/measurement"><VitalCards /></Route>
+        <Route path="/tracker"><TrackitCards /></Route>
+        <Route path="/progress"><ProgressPage /></Route>
+        <Route path="/menu"><Menu /></Route>
+        <Route path="/profile"><ProfilePage /></Route>
+      </Switch>
       <NavFooter />
     </>
   );
