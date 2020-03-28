ee58ebe0a7c36b1907a6fe399205be8c0355af5f
adriaanbd <adriaanbd@gmail.com>
2020-03-28 17:35:00 -0500

    removed underlined title; updated to single quotes

--- src/components/VitalCards.js
+++ src/components/VitalCards.js
@@ -12,7 +12,6 @@ import VitalCard from './VitalCard';
 import AddVitalCard from './AddVitalCard';
 import './styles/VitalsCards.css';
 import Layout from './Layout';
-import { Switch, Route } from 'react-router-dom';
 
 function VitalCards(props) {
   // title should come from redux state that gets it from api call
@@ -22,45 +21,45 @@ function VitalCards(props) {
         <div className="vitals-frame">
           <VitalCard
             icon={ <FontAwesomeIcon icon={faHeartbeat} size="3x" /> }
-            title={ "Systolic Pressure" }
-            className="vital-card"
-            id="systolic-card"
-            path="systolic"
+            title='Systolic Pressure'
+            className='vital-card'
+            id='systolic-card'
+            path='systolic'
           />
           <VitalCard
             icon={ <FontAwesomeIcon icon={faHeartbeat} size="3x" /> }
-            title={ "Diastolic Pressure" }
-            className="vital-card"
-            id="diastolic-card"
-            path="diastolic"
+            title='Diastolic Pressure'
+            className='vital-card'
+            id='diastolic-card'
+            path='diastolic'
           />
           <VitalCard
             icon={ <FontAwesomeIcon icon={faWeight} size="3x" /> }
-            title={ "Weight" }
-            className="vital-card"
-            id="weight-card"
-            path="weight"
+            title='Weight'
+            className='vital-card'
+            id='weight-card'
+            path='weight'
           />
           <VitalCard
             icon={ <FontAwesomeIcon icon={faSmile} size="3x" /> }
-            title={ "Mood" }
-            className="vital-card"
-            id="mood-card"
-            path="mood"
+            title='Mood'
+            className='vital-card'
+            id='mood-card'
+            path='mood'
           />
           <VitalCard
             icon={ <FontAwesomeIcon icon={faThermometerHalf} size="3x" /> }
-            title={ "Temperature" }
-            className="vital-card"
-            id="temperature-card"
-            path="temperature"
+            title='Temperature'
+            className='vital-card'
+            id='temperature-card'
+            path='temperature'
           />
           <VitalCard
             icon={ <FontAwesomeIcon icon={faAppleAlt} size="3x" /> }
-            title={ "Glucose" }
-            className="vital-card"
-            id="glucose-card"
-            path="glucose"
+            title='Glucose'
+            className='vital-card'
+            id='glucose-card'
+            path='glucose'
           />
         </div>
       </Layout>
