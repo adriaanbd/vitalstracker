10116655956c8342157e7801e1681e1b7e3a1978
adriaanbd <adriaanbd@gmail.com>
2020-03-29 16:24:58 -0500

    update vh to 80 bc ribbon and nav 20

--- src/components/AddVitalCard.js
+++ src/components/AddVitalCard.js
@@ -9,12 +9,14 @@ function AddVitalCard(props) {
   return (
     <>
       <Layout title="Add Vital" >
-        <div>
-          <h1 id="vital-card-title">{ title }</h1>
-        </div>
-        <div className="add-vital-card" id={id}>
-          <div id="vital-card-icon">{ icon }</div>
-          <AddVitalForm />
+        <div id="vital-card-main-wrap">
+          <div id="vital-card-title-wrap">
+            <h1 id="vital-card-title">{ title }</h1>
+          </div>
+          <div className="add-vital-card" id={id}>
+            <div id="vital-card-icon">{ icon }</div>
+            <AddVitalForm />
+          </div>
         </div>
       </Layout>
     </>
