04cce4db512687bc107905a2f5e706cba517b7e7
adriaanbd <adriaanbd@gmail.com>
2020-04-03 01:33:12 -0500

    api call to local host

--- src/api/calls.js
+++ src/api/calls.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const HEROKU_BASE_URL = 'http://lychee-cake-00846.herokuapp.com';
-// const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
+// const HEROKU_BASE_URL = 'http://lychee-cake-00846.herokuapp.com';
+const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
 
 export default async function sendRequest(method, path, data) {
-  const url = `${HEROKU_BASE_URL}/${path}`;
+  const url = `${LOCAL_BASE_URL}/${path}`;
   try {
     const resp = await axios[method](url, { ...data });
     return resp;
