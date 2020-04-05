fc339f86bee645c92a3a192856e31a0319f28741
adriaanbd <adriaanbd@gmail.com>
2020-04-05 12:09:31 -0500

    menu item text decoration non and color update

--- src/components/MenuPage/Header/MenuItem.js
+++ src/components/MenuPage/Header/MenuItem.js
@@ -5,16 +5,17 @@ import { Pane, Icon, Text } from 'evergreen-ui';
 function MenuItem(props) {
   const { relPath, icon, title } = props;
   return (
-    <Link to={`/menu/${relPath}`} className="menu-link">
+    <Link to={`/menu/${relPath}`} style={{ textDecoration: 'none' }}>
       <Pane
         display="flex"
         alignItems="center"
         backgroundColor="#e5eaee"
+        color="#67899C"
         paddingLeft={8}
         border="default"
         elevation={1}
       >
-        <Icon icon={props.icon} />
+        <Icon icon={icon} />
         <Text padding={16}>{title}</Text>
       </Pane>
     </Link>
