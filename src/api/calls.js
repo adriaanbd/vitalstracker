21f2eb387303ef65af98ff5b2359096484d52d75
adriaanbd <adriaanbd@gmail.com>
2020-04-06 12:43:06 -0500

    adding headers to api call;

--- src/api/calls.js
+++ src/api/calls.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
-// const HEROKU_BASE_URL = 'http://lychee-cake-00846.herokuapp.com';
-const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
+const HEROKU_BASE_URL = 'http://lychee-cake-00846.herokuapp.com';
+// const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
 
 export default async function sendRequest(method, path, data) {
-  const url = `${LOCAL_BASE_URL}/${path}`;
-  // const url = `${HEROKU_BASE_URL}/${path}`;
+  // const url = `${LOCAL_BASE_URL}/${path}`;
+  const URL = `${HEROKU_BASE_URL}/${path}`;
+  const HEADERS = {
+    Accept: 'application/json, text/plain, */*',
+    'Access-Control-Allow-Origin': '*',
+  };
   try {
-    const resp = await axios[method](url, { ...data });
+    const resp = await axios[method](URL, { ...data }, HEADERS);
     return resp;
   } catch (error) {
     return error;
