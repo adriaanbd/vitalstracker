cdec61466c174a0ba906a7026537697ee4053803
adriaanbd <adriaanbd@gmail.com>
2020-04-07 17:19:38 -0500

    moving api file to utils dir

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const HEROKU_BASE_URL = 'https://agile-oasis-62489.herokuapp.com/';
-// const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
+// const HEROKU_BASE_URL = 'https://agile-oasis-62489.herokuapp.com/';
+const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
 
 export default async function sendRequest(method, path, data) {
-  // const URL = `${LOCAL_BASE_URL}/${path}`;
-  const URL = `${HEROKU_BASE_URL}/${path}`;
+  const URL = `${LOCAL_BASE_URL}/${path}`;
+  // const URL = `${HEROKU_BASE_URL}/${path}`;
   const HEADERS = {
     Accept: 'application/json, text/plain, */*',
     'Access-Control-Allow-Origin': '*',
