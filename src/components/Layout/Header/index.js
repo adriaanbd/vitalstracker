823e35ad9501f051a5f3d30a92dc2372bfcfdd8a
adriaanbd <adriaanbd@gmail.com>
2020-04-08 16:10:53 -0500

    refactored header to use evergreen ui

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './HeaderRibbon.css';
 import { Link } from 'react-router-dom';
+import { Pane, Icon, Text } from 'evergreen-ui';
 
 function HeaderRibbon(props) {
   const { icon, title } = props;
   return (
-    <div className="ribbon">
-      <div className="ribbon-side" id="ribbon-left">
-        {icon ? <Link to="/menu" id="angle-left">{icon}</Link> : ''}
-      </div>
-      <div id="ribbon-title-box">
-        <h1 id="ribbon-title">{title || 'Track.It'}</h1>
-      </div>
-      <div className="ribbon-side" id="ribbon-right" />
-    </div>
+    <Pane
+      display="flex"
+      justifyContent="spaceBetween"
+      alignItems="center"
+      backgroundColor="#62b5e5"
+      color="whitesmoke"
+      height="10vh"
+      width="100vw"
+      elevation={1}
+    >
+      <Pane width="30%">
+        {icon ? (
+          <Link to="/menu" id="angle-left">
+            <Icon icon="chevron-left" marginLeft="0.4em" color="whitesmoke" size="1.5em" />
+          </Link>
+        ) : ''}
+      </Pane>
+      <Text
+        display="flex"
+        width="70%"
+        alignItems="center"
+        justifyContent="center"
+        fontSize="1.8em"
+        color="inherit"
+      >
+        {title}
+      </Text>
+      <Pane width="30%" id="ribbon-right" />
+    </Pane>
   );
 }
 
